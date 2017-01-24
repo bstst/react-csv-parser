4aa3a87ec4a904bc242bac9782a1e2ea2dd7097b
Martin Saulis <martin@saulis.com>
2017-01-24 16:08:28 +0200

    basic global styles (reset)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './src/app.js'
 
+import globalStyles from './src/global.css'
+
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
